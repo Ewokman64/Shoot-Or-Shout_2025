8c9a1be1fd50fbb96e0053e80956b6ee4210329f
Sandor Cserni <87653170+Ewokman64@users.noreply.github.com>
2025-03-25 16:04:45 +0100

    Shield ability 100% completed
    Shield ability works 100%.
    Added UI as SFX to it.
    Further balancing might be needed.

--- Assets/UseShield.cs
+++ Assets/UseShield.cs
@@ -11,9 +11,18 @@ public class UseShield : MonoBehaviour
 
     AudioManager audioManager;
 
+
+    public SpriteRenderer shieldUI_Renderer; //<- This is for the Shout ICON on the bottom.
+    public float darkenAmount = 0.5f; // Value between 0 and 1
+    private Color originalColor;
+
+
     private void Start()
     {
         audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        shieldUI_Renderer = GameObject.Find("ShieldSkillSprite").GetComponent<SpriteRenderer>();
+        // Get the current color of the sprite
+        originalColor = shieldUI_Renderer.color;
     }
     public void Update()
     {
@@ -25,6 +34,11 @@ public class UseShield : MonoBehaviour
         {
             shield.SetActive(true);
             audioManager.PlayShieldOn();
+            // Darken the color by multiplying it with a darker shade
+            Color darkenedColor = originalColor * darkenAmount;
+
+            // Set the darkened color to the sprite
+            shieldUI_Renderer.color = darkenedColor;
 
             StartCoroutine(ShieldOff());
         }
@@ -36,5 +50,7 @@ public class UseShield : MonoBehaviour
 
         shield.SetActive(false);
         audioManager.PlayShieldOff();
+
+        shieldUI_Renderer.color = originalColor;
     }
 }
